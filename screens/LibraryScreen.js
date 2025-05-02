3b83fd64522ff880f180a9d8d51e8204e7a00270
missopi <missopi@me.com>
2025-05-02 11:14:07 +0100

    Add basic code and style to library screen

---
+++ screens/LibraryScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function LibraryScreen() {
+  return (
+    <View style={StyleSheet.container}>
+      <Text>Activity Library</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
