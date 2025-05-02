fd934e9c27789dea9f71c205ba580786f43cca93
missopi <missopi@me.com>
2025-05-02 15:00:37 +0100

    Remove text field

--- screens/ToolsScreen.js
+++ screens/ToolsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 
 export default function ToolsScreen({ navigation }) {
   return (
