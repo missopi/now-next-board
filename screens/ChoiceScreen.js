e1662722a89b4a21c05d14eb5963d2340719659d
missopi <missopi@me.com>
2025-05-02 16:35:07 +0100

    Add custom button styling

--- screens/ChoiceScreen.js
+++ screens/ChoiceScreen.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import styles from './styles/styles';
+import CustomButton from './styles/CustomButton';
 
 export default function ChoiceScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Button title="I Feel..." onPress={() => navigation.navigate('Feelings Board')} />
-      <Button title="I Wan't..." onPress={() => navigation.navigate('Want Board')} />
-      <Button title="I Need..." onPress={() => navigation.navigate('Need Board')} />
-      <Button title='Activity Library' onPress={() => navigation.navigate('Library')} />
+      <CustomButton
+        title="I Feel..."
+        onPress={() => navigation.navigate('Feelings Board')}
+      />
+      <CustomButton
+        title="I Wan't..."
+        onPress={() => navigation.navigate('Want Board')}
+      />
+      <CustomButton
+        title="I Need..."
+        onPress={() => navigation.navigate('Need Board')}
+      />
+      <CustomButton
+        title="Activity Library"
+        onPress={() => navigation.navigate('Library')}
+      />
     </View>
   );
 }
