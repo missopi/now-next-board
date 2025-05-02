51da64769f44b9f8ba72620bc5c4249228a898b0
missopi <missopi@me.com>
2025-05-02 16:11:20 +0100

    Trial adding a pressable button to see how it looks

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Text, Pressable, Button } from 'react-native';
 import styles from './styles';
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Button title='Today' onPress={() => navigation.navigate('Today')} />
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed
+        ]}
+        onPress={() => navigation.navigate('Today')}
+      >
+        <Text style={styles.buttonText}>Today</Text>
+      </Pressable>
       <Button title='Now/Next Boards' onPress={() => navigation.navigate('Now/Next Board')} />
       <Button title='Choice Boards' onPress={() => navigation.navigate('Choice Boards')} />
       <Button title='Tools' onPress={() => navigation.navigate('Tools')} />
