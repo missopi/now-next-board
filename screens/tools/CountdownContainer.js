25a85885d5442ea86de2853adc042ab3e493fae2
missopi <missopi@me.com>
2025-05-03 19:57:28 +0100

    Add option to choose activity before countdown

--- screens/tools/CountdownContainer.js
+++ screens/tools/CountdownContainer.js
@@ -3,18 +3,13 @@ import CountdownSetup from "./CountdownSetup";
 import Countdown from "./Countdown";
 
 const CountdownContainer = () => {
-  const [startCount, setStartCount] = useState(null);
+  const [startData, setStartData] = useState(null);
 
-  const activity = {
-    name: '',
-    image: require('../images/hair_cut.jpg'),
-  };
-
-  if (startCount !== null) {
-    return <Countdown countStart={startCount} activity={activity} />;
+  if (startData) {
+    return <Countdown countStart={startData.count} activity={startData.activity} />;
   }
 
-  return <CountdownSetup onStart={setStartCount} />;
+  return <CountdownSetup onStart={(count, activity) => setStartData({ count, activity })} />;
 };
 
 export default CountdownContainer;
--- screens/tools/CountdownSetup.js
+++ screens/tools/CountdownSetup.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { View, Text, Button, Pressable, TextInput } from "react-native";
+import { View, Text, Button, Pressable, TextInput, TouchableOpacity, Image, FlatList } from "react-native";
 import styles from "../styles/styles";
 
 const CountdownSetup = ({ onStart }) => {
   const [count, setCount] = useState(10);
+  const [selectedActivity, setSelectedActivity] = useState(null);
+
+  const activityLibrary = [
+    { id: 1, name: 'Hair Cut', image: require('../images/hair_cut.jpg') },
+    { id: 2, name: 'Cut Nails', image: require('../images/cut_nails.jpg') },
+    { id: 3, name: 'Wash Hands', image: require('../images/wash_hands.jpg') },
+  ];
 
   const handleChange = (text) => {
     if (/^\d+$/.test(text) || text === '') {  // Allow only digits or empty input
@@ -21,10 +28,30 @@ const CountdownSetup = ({ onStart }) => {
         keyboardType="numeric"
         placeholder="Enter countdown value"
       />
+
+      <Text style={styles.label}>Choose Activity:</Text>
+      <FlatList
+        vertical
+        data={activityLibrary}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={[
+              styles.activityCard,
+              selectedActivity?.id === item.id && styles.selectedCard,
+            ]}
+            onPress={() => setSelectedActivity(item)}
+          >
+            <Image source={item.image} style={styles.activityImage} />
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
       <Button
         title="Start Countdown"
-        onPress={() => onStart(count)}
-        disabled={!Number.isInteger(count) || count <= 0 }
+        onPress={() => onStart((count), selectedActivity)}
+        disabled={!Number.isInteger(count) || count <= 0 || !selectedActivity}
       />
     </View>
   );
