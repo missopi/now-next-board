3e85c47872d3738f960bae521e88d4d890f8857b
missopi <missopi@me.com>
2025-05-04 13:49:32 +0100

    Add shared activity library

---
+++ screens/ActivityLibrary.js
@@ -0,0 +1,17 @@
+export const activityLibrary = [
+  {
+    id: '1',
+    name: 'Hair Cut',
+    image: require('./images/hair_cut.jpg')
+  },
+  {
+    id: '2',
+    name: 'Cut Nails',
+    image: require('./images/cut_nails.jpg')
+  },
+  {
+    id: '3',
+    name: 'Wash Hands',
+    image: require('./images/wash_hands.jpg')
+  },
+]
