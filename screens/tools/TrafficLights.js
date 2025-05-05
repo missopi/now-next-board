c9a78d8afe4dbfcd2ccb3d7f2ef5c121a367d095
missopi <missopi@me.com>
2025-05-05 17:08:13 +0100

    Adjust margin on finished text

--- screens/tools/TrafficLights.js
+++ screens/tools/TrafficLights.js
@@ -5,7 +5,7 @@ import styles from "../styles/styles";
 const trafficLightData = [
   { backgroundColor: "#7dbf7d", textColor: "#4e8e4e", text: "Starting", marginTop: 150 },
   { backgroundColor: "#e0a958", textColor: "#b27833", text: "Nearly Finished", marginTop: 310 },
-  { backgroundColor: "#d96c6c", textColor: "#a54242", text: "Finished", marginTop: 550 },
+  { backgroundColor: "#d96c6c", textColor: "#a54242", text: "Finished", marginTop: 600 },
 ];
 
 const TrafficLights = () => {
