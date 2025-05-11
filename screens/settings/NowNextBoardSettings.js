698b0d491b2cb99cccaeca7d79e865aead2a9af8
missopi <missopi@me.com>
2025-05-11 17:30:18 +0100

    Remove code no longer needed

--- screens/settings/NowNextBoardSettings.js
+++ screens/settings/NowNextBoardSettings.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, Switch, TouchableOpacity } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import styles from "../styles/NowNextBoardStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
