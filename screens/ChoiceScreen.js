82a717007ef795f2f670400c745175c2bd9e18ab
missopi <missopi@me.com>
2025-05-15 17:11:08 +0100

    Remove link to settings in choice menu for now.

--- screens/ChoiceScreen.js
+++ screens/ChoiceScreen.js
@@ -1,10 +1,9 @@
-import React, {useLayoutEffect, useCallback} from "react";
-import { View, Pressable } from 'react-native';
+import React, { useCallback } from "react";
+import { View } from 'react-native';
 import styles from './styles/styles';
 import CustomButton from './styles/CustomButton';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { useFocusEffect } from '@react-navigation/native';
-import CogIcon from '../assets/icons/cog.svg';
 
 export default function ChoiceScreen({ navigation }) {
   useFocusEffect(
@@ -16,16 +15,6 @@ export default function ChoiceScreen({ navigation }) {
       };
     }, [])
   );
-
-  useLayoutEffect(() => {
-      navigation.setOptions({
-        headerRight: () => (
-          <Pressable onPress={() => navigation.navigate('HomeSettings')} style={{ marginRight: 15 }}>
-            <CogIcon width={24} height={24} />
-          </Pressable>
-        ),
-      });
-  }, [navigation]);
 
   return (
     <View style={styles.container}>
