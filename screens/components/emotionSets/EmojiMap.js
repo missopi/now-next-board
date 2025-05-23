ecd353412ad069f6d60335f623125d88f4c8e1c0
missopi <missopi@me.com>
2025-05-23 15:30:14 +0100

    Move emoji map to its own file so is cleaner and can add additional emotion sets for users to choose from.

---
+++ screens/components/emotionSets/EmojiMap.js
@@ -0,0 +1,43 @@
+// SVG imports
+import Amused from "../../../assets/emojiEmotions/amused.svg";
+import Angry from "../../../assets/emojiEmotions/angry.svg";
+import Confused from "../../../assets/emojiEmotions/confused.svg";
+import Happy from "../../../assets/emojiEmotions/happy.svg";
+import Sad from "../../../assets/emojiEmotions/sad.svg";
+import Hurt from "../../../assets/emojiEmotions/hurt.svg";
+import Silly from "../../../assets/emojiEmotions/silly.svg";
+import Sick from "../../../assets/emojiEmotions/sick.svg";
+import Tired from "../../../assets/emojiEmotions/tired.svg";
+import Upset from "../../../assets/emojiEmotions/upset.svg";
+import Shocked from "../../../assets/emojiEmotions/shocked.svg";
+import Scared from "../../../assets/emojiEmotions/scared.svg";
+import Hot from "../../../assets/emojiEmotions/hot.svg";
+import Cold from "../../../assets/emojiEmotions/cold.svg";
+import Embarressed from "../../../assets/emojiEmotions/embarressed.svg";
+import Worried from "../../../assets/emojiEmotions/worried.svg";
+import Cheeky from "../../../assets/emojiEmotions/cheeky.svg";
+import Annoyed from "../../../assets/emojiEmotions/annoyed.svg";
+
+// List of emotion emoji icons and labels
+const emojiSet = [
+  {id: '1', Icon: Happy, label: 'happy'},
+  {id: '2', Icon: Sad, label: 'sad'},
+  {id: '3', Icon: Angry, label: 'angry'},
+  {id: '4', Icon: Hurt, label: 'hurt'},
+  {id: '5', Icon: Silly, label: 'silly'},
+  {id: '6', Icon: Sick, label: 'sick'},
+  {id: '7', Icon: Tired, label: 'tired'},
+  {id: '8', Icon: Upset, label: 'upset'},
+  {id: '9', Icon: Amused, label: 'amused'},
+  {id: '10', Icon: Shocked, label: 'shocked'},
+  {id: '11', Icon: Scared, label: 'scared'},
+  {id: '12', Icon: Hot, label: 'hot'},
+  {id: '13', Icon: Cold, label: 'cold'},
+  {id: '14', Icon: Embarressed, label: 'embarressed'},
+  {id: '15', Icon: Confused, label: 'confused'},
+  {id: '16', Icon: Worried, label: 'worried'},
+  {id: '17', Icon: Cheeky, label: 'cheeky'},
+  {id: '18', Icon: Annoyed, label: 'annoyed'},
+];
+
+export default emojiSet;
