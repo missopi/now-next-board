3ea518f950cabb3bdb9b788668ed3e193c621bf6
missopi <missopi@me.com>
2025-05-24 19:49:07 +0100

    Add place to fetch saved boards

--- screens/savedScreen.js
+++ screens/savedScreen.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import { Text, FlatList, TouchableOpacity } from "react-native";
+import { getBoards } from "./components/BoardStore";
 
-const SavedScreen = () => {
+
+const SavedScreen = ({ onBoardSelected }) => {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+      const all = await getBoards();
+      setBoards(all);
+    };
+    fetchBoards();
+  }, []);
+
   return (
-    <View>
-      <Footer />
-    </View>
+    <FlatList
+      data={boards}
+      keyExtractor={(b) => b.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => onBoardSelected(item)}>
+          <Text>{item.title} ({item.type})</Text>
+        </TouchableOpacity>
+      )}
+    />
   );
 };
-
-export default SavedScreen;
