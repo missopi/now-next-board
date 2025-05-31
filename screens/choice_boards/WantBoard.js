a27cf5f350f4e06086f50dacf8beebfb831692da
missopi <missopi@me.com>
2025-05-31 15:44:12 +0100

    Add header text and styling to wants and needs boards

--- screens/choice_boards/WantBoard.js
+++ screens/choice_boards/WantBoard.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Image, Modal } from "react-native";
+import { View, Text, TouchableOpacity, SafeAreaView, Modal } from "react-native";
 import styles from '../styles/ChoiceBoardStyles';
 import CogIcon from "../../assets/icons/cog.svg";
-import Footer from "../components/Footer";
 
 const WantBoard = ({ navigation }) => {
   const [settingsVisible, setSettingsVisible] = useState(false);
@@ -18,8 +17,26 @@ const WantBoard = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Want Board</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ paddingHorizontal: 30, paddingTop: 10 }}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', paddingBottom: 10 }}>Want Board</Text>
+        <Text style={{ paddingBottom: 10 }}>Select up to eight wants to include on your "I want..." board.</Text>
+
+        <TouchableOpacity
+          style={[
+            styles.createBoardButton,
+            { borderColor: '#999' },
+          ]}
+        >
+          <Text 
+            style={[
+              styles.createBoardButtonText,
+              { color: '#999' },
+            ]}
+          >Create Board</Text>
+        </TouchableOpacity>
+      </View>
+
       <Modal  // setting for toggling on 'then' activity at bottom of screen
         visible={settingsVisible}
         transparent={true}
@@ -32,7 +49,7 @@ const WantBoard = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       </Modal>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- screens/styles/ChoiceBoardStyles.js
+++ screens/styles/ChoiceBoardStyles.js
@@ -2,7 +2,18 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
 
-  
+  createBoardButton: {
+    borderWidth: 3,
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginHorizontal: 50,
+    marginBottom: 10,
+  },
+  createBoardButtonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
 
   // modal
 
@@ -24,25 +35,24 @@ export default StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     width: '100%',
-},
-modal: {
-  justifyContent: 'flex-end',
-  margin: 0,
-  flex: 1,
-},
-modalText: {
+  },
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+    flex: 1,
+  },
+  modalText: {
     fontSize: 14,
-},
-closeButton: {
-  position: 'absolute',
-  top: 15,
-  right: 30,
-  zIndex: 1,
-},
-closeX: {
-  fontSize: 28,
-  fontWeight: 400,
-  color: '#000',
-},
-
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 15,
+    right: 30,
+    zIndex: 1,
+  },
+  closeX: {
+    fontSize: 28,
+    fontWeight: 400,
+    color: '#000',
+  },
 });
