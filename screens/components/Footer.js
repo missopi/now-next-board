f5e6c99ed26254232e7d355f95558fac78d12391
missopi <missopi@me.com>
2025-09-29 15:46:58 +0100

    Add footer js file with links to routines, now & next and saved to go on each page

---
+++ screens/components/Footer.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Pressable, Text } from "react-native";
+import { useNavigation, useNavigationState } from "@react-navigation/native";
+import styles from "../styles/styles";
+import TimerIcon from "../../assets/icons/timer.svg";
+import ArrowIcon from "../../assets/icons/star.svg";
+import SaveIcon from "../../assets/icons/heart.svg";
+
+const Footer = () => {
+  const navigation = useNavigation();
+
+  const currentRoute = useNavigationState((state) => { // get current route reliably, was mismatching before
+    return state.routes[state.index].name;
+  });
+
+  const options = [
+    { name: 'routines', icon: TimerIcon, route: 'Routines', matchRoutes: ['Routines'] },
+    { name: 'now & next', icon: ArrowIcon, route: 'Now/Next', matchRoutes: ['Now/Next']},
+    { name: 'saved', icon: SaveIcon, route: 'AllBoardsScreen', matchRoutes: ['AllBoardsScreen'] },
+  ];
+
+  return (
+    <View style={styles.footer}>
+      {options.map((option) => {
+
+        const isActive = option.matchRoutes.includes(currentRoute);
+        const Icon = option.icon;
+
+        return (
+          <Pressable
+            key={option.name}
+            onPress={() => navigation.navigate(option.route)}
+            style={styles.footerButton}
+          >
+            <Icon width={24} height={24} fill={isActive ? '#000' : '#999'} />
+            <Text style={[styles.footerLabel, isActive && styles.activeFooterLabel]}>
+              {option.name}
+            </Text>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+};
+
+export default Footer;
--- screens/styles/styles.js
+++ screens/styles/styles.js
@@ -10,36 +10,6 @@ export default StyleSheet.create({
     position: 'relative',
     backgroundColor: '#fff',
   },
-  button: {
-    paddingVertical: 25,
-    paddingHorizontal: 16,
-    borderRadius: 15,
-    marginVertical: 8,
-    width: '75%', // stretch button
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 5, // for android
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: '600',
-    textAlign: 'center',
-  },
-  buttonContent: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: '#333',
-  },
-  icon: {
-    marginRight: 8,
-  },
 
   // library
 
@@ -55,65 +25,13 @@ export default StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
   },
-
   activityName: {
     fontSize: 20,
     fontWeight: '600',
     color: '#333',
   },
-  
-// Settings Modal
 
-  handle: {
-    backgroundColor: '#ccc',
-    width: 40,
-    height: 5,
-    borderRadius: 2.5,
-    alignSelf: 'center',
-    marginBottom: 10, // adds space between handle and first item
-  },
-  modal: {
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    paddingBottom: 40,
-    padding: 30,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontWeight: 'bold',
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  modalRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  radioCircle: {
-    height: 20,
-    width: 20,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 12,
-  },
-  radioDot: {
-    height: 10,
-    width: 10,
-    borderRadius: 5,
-    backgroundColor: '#000',
-  },
-  modalLabel: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  modalText: {
-    fontSize: 14,
-  },
+  // home screen
   savedButton: {
     width: 325,
     height: 100,
@@ -153,32 +71,6 @@ export default StyleSheet.create({
     shadowRadius: 3,
     elevation: 5, // for android
   },
-  settingButton: {
-    backgroundColor: '#ff5f8e',
-    width: 100,
-    height: 100,
-    borderRadius:12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 5, // for android
-  },
-  choiceButton: {
-    backgroundColor: '#123456',
-    width: 220,
-    height: 220,
-    borderRadius:12,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 5, // for android
-  },
   savedText: {
     fontSize: 30,
     fontWeight: '500',
@@ -199,5 +91,39 @@ export default StyleSheet.create({
     flexDirection: 'column',
     gap: 12,
     marginBottom: 10,
+  },
+
+  // footer
+
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+    backgroundColor: '#eee',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    paddingBottom: 20,
+  },
+  footerButton: {
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    flex: 1,
+  },
+  footerLabel: {
+    fontSize: 12,
+    color: '#999',
+    marginTop: 2,
+    marginLeft: 6,
+  },
+  activeFooterLabel: {
+    color: '#000',
   },
 });
