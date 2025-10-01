1fc1de2ddd09b669a73ff6274a9b2d6b9db51929
missopi <missopi@me.com>
2025-10-01 16:01:11 +0100

    Add logo to homescreen and adjust button colour to match logo colour.  Save additional image files of logo wording and icon too.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,38 +1,16 @@
-import { useCallback, useRef } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import * as ScreenOrientation from 'expo-screen-orientation';
-import { useFocusEffect } from '@react-navigation/native';
+import { useRef } from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import styles from './styles/styles';
+import AndNext from "../assets/andNext-logo";
 
 export default function HomeScreen({ navigation }) {
   const modalRef = useRef(null);
 
-  useFocusEffect(
-    useCallback(() => {
-      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
-
-      return () => {
-        ScreenOrientation.unlockAsync();
-      };
-    }, [])
-  );
-
   return (
     <View style={styles.container}>
-      
-      <View style={styles.buttonRow}>
-        <TouchableOpacity style={styles.routinesButton} onPress={() => navigation.navigate('Routines')}>
-          <Text style={styles.routineText}>Routines</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.nowButton} onPress={() => navigation.navigate('Now/Next')}>
-          <Text style={styles.savedText}>Now & Next</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.buttonRow}>
-        <TouchableOpacity style={styles.savedButton} onPress={() => navigation.navigate('AllBoardsScreen')}>
-          <Text style={styles.savedText}>Saved Boards</Text>  
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Now/Next')}>
+        <AndNext width={280} />
+      </TouchableOpacity>
     </View>
   );
 }
