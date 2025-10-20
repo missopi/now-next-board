6c66649dcfaaa4a88fdf5aab907c19c6a41027c4
missopi <missopi@me.com>
2025-10-20 16:23:04 +0100

    Add title of routine to top of page

--- screens/FinishedBoards/FinishedRoutine.js
+++ screens/FinishedBoards/FinishedRoutine.js
@@ -1,9 +1,9 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import DraggableFlatList from "react-native-draggable-flatlist";
 import RoutineCard from "../components/RoutineCard";
 import styles from "../styles/RoutineStyles";
 
-export default function FinishedRoutine({ route }) {
+export default function RoutineViewScreen({ route }) {
   const { board } = route.params || {};
 
   if (!board) return null;
@@ -15,18 +15,44 @@ export default function FinishedRoutine({ route }) {
       <DraggableFlatList
         data={activities}
         keyExtractor={(item) => item.id}
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={false}
+        onDragEnd={() => {}} // no dragging
         renderItem={({ item }) => (
           <RoutineCard
             activity={item}
-            index={item.id}
-            readOnly={true}  // disables edit/delete/drag
+            readOnly={true} // hide delete + drag buttons
           />
         )}
-        scrollEnabled={true}
-        showsVerticalScrollIndicator={false}
-        // disable dragging
-        onDragEnd={() => {}}
+
+        // 👇 Add header to scroll with list
+        ListHeaderComponent={() => (
+          <View style={{ alignItems: "center", marginVertical: 5 }}>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: "600",
+                color: "#333",
+                textAlign: "center",
+              }}
+            >
+              {board.title || "Routine"}
+            </Text>
+            <View
+              style={{
+                height: 2,
+                width: "60%",
+                backgroundColor: "#ccc",
+                marginTop: 8,
+              }}
+            />
+          </View>
+        )}
+
+        contentContainerStyle={{ paddingBottom: 80 }}
       />
     </View>
   );
 }
+
+
