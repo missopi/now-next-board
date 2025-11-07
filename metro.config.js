ba10adaa025f4dc25300dff4e3044e8d42ab398a
missopi <missopi@me.com>
2025-11-07 11:48:44 +0000

    Update expo and dependancies

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,9 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+const config = getDefaultConfig(__dirname);
 
-module.exports = (async () => {
-  const config = getDefaultConfig(__dirname);
-  const { transformer, resolver } = config;
-  
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
-  };
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts.push('svg');
 
-  return config;
-})();
+module.exports = config;
