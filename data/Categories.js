f96a75855b0b0a0c0aba319f04d00aaff785f0a7
missopi <missopi@me.com>
2026-01-11 15:10:11 +0000

    Add additional categories for image library

--- data/Categories.js
+++ data/Categories.js
@@ -1,21 +1,14 @@
 export const allCategories = [
   { label: 'All', key: 'All', visible: true },
   { label: 'Personal Care', key: 'Personal Care', visible: true },
-  { label: 'Food', key: 'Food', visible: true },
+  { label: 'Activites', key: 'Activities', visible: true },
+  { label: 'Food & Drink', key: 'Food & Drink', visible: true },
+  { label: 'People', key: 'People', visible: true },
+  { label: 'Clothing', key: 'Clothing', visible: true },
   { label: 'Holiday & Travel', key: 'Holiday & Travel', visible: true },
   { label: 'Places', key: 'Places', visible: true },
   { label: 'Medical', key: 'Medical', visible: true },
   { label: 'Early Years', key: 'Early Years', visible: true },
   { label: 'School', key: 'School', visible: true },
+  { label: 'Calming', key: 'Calming', visible: true },
 ];
-
-export const defaultCategories = [
-  { key: 'Personal Care', label: 'Personal Care', visible: true },
-  { key: 'Food', label: 'Food', visible: true },
-  { key: 'Holiday & Travel', label: 'Holiday & Travel', visible: false },
-  { key: 'Places', label: 'Places', visible: false },
-  { key: 'Medical', label: 'Medical', visible: false },   
-  { key: 'Early Years', label: 'Early Years', visible: false },
-  { key: 'School', label: 'School', visible: false },
-];
-
